94cd9d0a686c0ebf46413b982808fb1791134643
Asfand-Khan <khanasfand174@gmail.com>
2024-10-20 22:54:59 +0500

    fix bugs

--- src/app/case-study/add/page.tsx
+++ src/app/case-study/add/page.tsx
@@ -263,7 +263,7 @@ const Page = () => {
                   <Image
                     width={100}
                     height={100}
-                    src={`https://res.cloudinary.com/dkkxjlzsr/image/upload/${caseStudy.image}`}
+                    src={`${process.env.CLOUDINARY_ASSETS_ACCESS_URL}/${caseStudy.image}`}
                     alt={`case study ${index}`}
                   />
                 </div>
